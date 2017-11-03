71ef73a27b0e497660e936a43c98fb2f6e18c139
Dusty McCord <dustymccord@gmail.com>
2017-11-03 09:10:13 -0700

    added validation for email domain and a settings link to home page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    validates_format_of :email, with: /\@email\.com/, message: 'You should have an email from email.com'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
