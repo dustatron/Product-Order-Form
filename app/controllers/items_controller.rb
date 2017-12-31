c0f1c0f7cb46d727a56d06b4dc57abc6541d9229
Dusty McCord <dustymccord@gmail.com>
2017-12-31 13:17:33 -0700

    tweeked controller for items.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,28 @@
 class ItemsController < ApplicationController
 
   def show
+
   end
 
   def create
-    @item = Item.new({
+    order_number = params(:id)
+    @item = Item.new(params_items)
 
-      })
-
+    if @item.save
+      flash[:success] = "Item Saved."
+      redirect_to root_path
+    else
+      flash[:danger] = "Error Occured. Item not saved."
+    end
   end
+
+  def edit
+    order_number = params(:id)
+    @item = Item.where(id: order_number)
+  end
+
+  private
+    def params_items
+      params.require(:item).permit(:artical_number, :artical_size, :artical_gender)
+    end
 end
